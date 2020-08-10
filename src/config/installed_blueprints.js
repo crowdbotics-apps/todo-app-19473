a4b565b27e4b22156ff79cb06f0fd6e376386ac2
Crowdbotics <team@crowdbotics.com>
2020-08-10 15:56:59 +0000

    remove package blueprint-react-native-profiles-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -4,7 +4,6 @@ export const installed_blueprints = [
 { name: 'Maps89410', human_name: 'Maps', access_route: 'Maps89410', icon: 'map'},
 { name: 'Add-Item89409', human_name: 'Add-item', access_route: 'Add-Item89409'},
 { name: 'Maps89405', human_name: 'Maps', access_route: 'Maps89405', icon: 'map'},
-{ name: 'UserProfile89401', human_name: 'User Profile', access_route: 'UserProfile89401'},
 
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
